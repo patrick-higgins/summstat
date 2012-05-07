16ec416f8a635f8dce31b1a319c40f57c5534331
Patrick Higgins <patrick.allen.higgins@gmail.com>
2012-05-07 16:19:16 -0600

    Initial commit.

---
+++ stat.go
@@ -0,0 +1,223 @@
+package summstat
+
+import (
+	"math"
+	"sort"
+)
+
+// The type of samples we track statistics for
+type Sample float64
+
+// Implements sort.Interface so that we can sort samples for percentiles
+type sampleSlice []Sample
+
+func (s sampleSlice) Len() int {
+	return len(s)
+}
+
+func (s sampleSlice) Less(i, j int) bool {
+	return s[i] < s[j]
+}
+
+func (s sampleSlice) Swap(i, j int) {
+	s[i], s[j] = s[j], s[i]
+}
+
+// A Stats represents descriptive statistics about Samples which are being
+// added incrementally.
+type Stats struct {
+	count     int
+	sum       Sample
+	sum2      Sample
+	max       Sample
+	min       Sample
+	samples   []Sample
+	sorted    bool
+	bins      []Sample
+	binCounts []int
+}
+
+// NewStats returns a new Stats
+func NewStats() *Stats {
+	return &Stats{
+		max: -math.MaxFloat64,
+		min: math.MaxFloat64,
+	}
+}
+
+// AddSample adds a sample value and updates the statistics.
+func (s *Stats) AddSample(val Sample) {
+	s.count++
+	s.sum += val
+	s.sum2 += val * val
+	if val > s.max {
+		s.max = val
+	}
+	if val < s.min {
+		s.min = val
+	}
+	if len(s.bins) > 0 {
+		for bin, binVal := range s.bins {
+			if val <= binVal {
+				s.binCounts[bin]++
+			}
+		}
+	} else {
+		s.samples = append(s.samples, val)
+		s.sorted = false
+	}
+}
+
+// Count returns the number of samples added.
+func (s Stats) Count() int {
+	return s.count
+}
+
+// Min returns minimal sample value added.
+func (s Stats) Min() Sample {
+	if s.min > s.max {
+		return 0
+	}
+	return s.min
+}
+
+// Max returns the maximal sample value added.
+func (s Stats) Max() Sample {
+	if s.min > s.max {
+		return 0
+	}
+	return s.max
+}
+
+func (s *Stats) sortSamples() {
+	if !s.sorted {
+		sort.Sort(sampleSlice(s.samples))
+		s.sorted = true
+	}
+}
+
+// Percentile returns the sample value at the given percentile.
+//
+// It may not be called after CreateBins, which discards the samples from
+// which the percentile is calculated.
+func (s Stats) Percentile(pct float64) Sample {
+	if len(s.bins) > 0 {
+		panic("cannot call Percentile() after CreateBins()")
+	}
+	if len(s.samples) == 0 {
+		return 0
+	}
+	if pct < 0 {
+		panic("pct too small")
+	}
+	if pct > 1 {
+		panic("pct too large")
+	}
+	s.sortSamples()
+	// scale pct into int in [0, len-1]
+	// Adding 0.5 turns the implicit floor operation of int() into a rounding operation
+	i := int(float64(len(s.samples)-1)*pct + 0.5)
+	return s.samples[i]
+}
+
+// Median returns the median of the samples.
+//
+// It may not be called after CreateBins, which discards the samples from
+// which the percentile is calculated.
+func (s Stats) Median() float64 {
+	if len(s.bins) > 0 {
+		panic("cannot call Percentile() after CreateBins()")
+	}
+	l := len(s.samples)
+	if l == 0 {
+		return 0
+	}
+	s.sortSamples()
+	half, rem := l/2, l%2
+	if rem == 0 {
+		return (float64(s.samples[half]) + float64(s.samples[half-1])) / 2
+	}
+	return float64(s.samples[half])
+}
+
+// Mean returns the mean of the samples.
+func (s Stats) Mean() float64 {
+	return float64(s.sum) / float64(s.count)
+}
+
+// Stddev returns the standard deviation of the samples.
+func (s Stats) Stddev() float64 {
+	m := s.Mean()
+	return math.Sqrt(float64(s.sum2)/float64(s.count) - m*m)
+}
+
+// Spread returns the difference of the maximal and minimal sample values.
+func (s Stats) Spread() Sample {
+	if s.min > s.max {
+		return 0
+	}
+	return s.max - s.min
+}
+
+// CreateBins divides the sample space into nbins bins for tracking counts.
+//
+// As samples are added, the count for the corresponding bin will be
+// incremented and the sample value will not be stored.
+//
+// This saves memory at the expense of granularity. Percentile() and Median()
+// cannot be called after CreateBins() because they are no longer meaningful.
+// Instead, use Bin(i) to inspect the distribution of data by bin. Any existing
+// stored samples are discarded.
+//
+// The bins created will be:
+//   (-Inf,low], (low, s/nmid+low], (s/nmid+low, 2s/nmid], ..., (high,+Inf)
+//   where:
+//     s = high - low
+//     nmid = nbins-2
+//
+// Thus, the space (high-low) is divided into nbins-2 equally sized pieces
+// and the remaining two bins extend from -math.MaxFloat64 to low and high to
+// math.MaxFloat64.
+//
+// Low must be strictly less than high, so nbins must be at least 3.
+func (s *Stats) CreateBins(nbins int, low, high Sample) {
+	if high <= low {
+		panic("high must be greater than low")
+	}
+	if nbins < 3 {
+		panic("Not enough bins")
+	}
+	spread := high - low
+	s.bins = make([]Sample, nbins)
+	for i := 0; i < nbins-1; i++ {
+		s.bins[i] = Sample(i)*spread/Sample(nbins-2) + low
+	}
+	s.bins[nbins-1] = math.MaxFloat64
+	// save memory: stop storing samples now that we track by bins
+	s.samples = []Sample{}
+}
+
+// CreateBinsDiscard is shorthand for calling CreateBins(nbins, ...) with low
+// value s.Percentile(discardPct) and high value s.Percentile(1-discardPct)
+// with a check to make sure enough samples have been collected to make
+// discardPct meaningful (1/discardPct samples are required).
+func (s *Stats) CreateBinsDiscard(nbins int, discardPct float64) {
+	if len(s.samples) < int(1.0/discardPct) {
+		panic("Not enough samples")
+	}
+	s.CreateBins(nbins, s.Percentile(discardPct), s.Percentile(1.0-discardPct))
+}
+
+// Returns the count and low and high ends of the i'th bin.
+//
+// The bin interval is (low,high]
+func (s Stats) Bin(i int) (count int, low, high Sample) {
+	count = s.binCounts[i]
+	high = s.bins[i]
+	if i == 0 {
+		low = -math.MaxFloat64
+	} else {
+		low = s.bins[i-1]
+	}
+	return
+}
---
+++ stat_test.go
@@ -0,0 +1,283 @@
+package summstat
+
+import (
+	"math"
+	"testing"
+)
+
+const (
+	epsilon = 0.0000000000000000001
+)
+
+type statTest struct {
+	count   int
+	min     Sample
+	max     Sample
+	median  float64
+	mean    float64
+	stddev  float64
+	spread  Sample
+	samples []Sample
+}
+
+var (
+	tests = []statTest{
+		{ // 0
+			samples: []Sample{},
+			count:   0,
+			min:     0,
+			max:     0,
+			median:  0,
+			mean:    0,
+			stddev:  0,
+			spread:  0,
+		},
+		{ // 1
+			samples: []Sample{1},
+			count:   1,
+			min:     1,
+			max:     1,
+			median:  1,
+			mean:    1,
+			stddev:  0,
+			spread:  0,
+		},
+		{ // 2
+			samples: []Sample{1, 2},
+			count:   2,
+			min:     1,
+			max:     2,
+			median:  1.5,
+			mean:    1.5,
+			stddev:  .5,
+			spread:  1,
+		},
+		{ // 3
+			samples: []Sample{1, 2, 3},
+			count:   3,
+			min:     1,
+			max:     3,
+			median:  2,
+			mean:    2,
+			stddev:  0.8164965809277263,
+			spread:  2,
+		},
+		{ // 4
+			samples: []Sample{0, 1, 2, 3, 4, 5},
+			count:   6,
+			min:     0,
+			max:     5,
+			median:  2.5,
+			mean:    2.5,
+			stddev:  1.707825127659933,
+			spread:  5,
+		},
+		{ // 5
+			samples: []Sample{-10, -9, -8, -7},
+			count:   4,
+			min:     -10,
+			max:     -7,
+			median:  -8.5,
+			mean:    -8.5,
+			stddev:  1.118033988749895,
+			spread:  3,
+		},
+		{ // 6
+			samples: []Sample{-1, 1},
+			count:   2,
+			min:     -1,
+			max:     1,
+			median:  0,
+			mean:    0,
+			stddev:  1,
+			spread:  2,
+		},
+		{ // 7
+			samples: []Sample{-1, 0, 1},
+			count:   3,
+			min:     -1,
+			max:     1,
+			median:  0,
+			mean:    0,
+			stddev:  0.816496580927726,
+			spread:  2,
+		},
+	}
+)
+
+func insertSamples(s *Stats, samples []Sample) {
+	for _, sample := range samples {
+		s.AddSample(sample)
+	}
+}
+
+func Test(t *testing.T) {
+	for i, test := range tests {
+		s := NewStats()
+		insertSamples(s, test.samples)
+		if s.Count() != test.count {
+			t.Errorf("[%d] Invalid count: %d, expected: %d", i, s.Count(), test.count)
+		}
+		if s.Min() != test.min {
+			t.Errorf("[%d] Invalid min: %v, expected: %v", i, s.Min(), test.min)
+		}
+		if s.Max() != test.max {
+			t.Errorf("[%d] Invalid max: %v, expected: %v", i, s.Max(), test.max)
+		}
+		if s.Median() != test.median {
+			t.Errorf("[%d] Invalid median: %v, expected: %v", i, s.Median(), test.median)
+		}
+		if math.Abs(s.Mean()-test.mean) > epsilon {
+			t.Errorf("[%d] Invalid mean: %f, expected: %f", i, s.Mean(), test.mean)
+		}
+		if math.Abs(s.Stddev()-test.stddev) > epsilon {
+			t.Errorf("[%d] Invalid stddev: %v, expected: %v", i, s.Stddev(), test.stddev)
+		}
+		if s.Spread() != test.spread {
+			t.Errorf("[%d] Invalid spread: %v, expected: %v", i, s.Spread(), test.spread)
+		}
+	}
+}
+
+func chkPct(t *testing.T, s *Stats, pct float64, exp Sample) {
+	if s.Percentile(pct) != exp {
+		t.Errorf("%.1f%% != %v: %v", 100*pct, exp, s.Percentile(pct))
+	}
+}
+
+func TestPercentile(t *testing.T) {
+	s := NewStats()
+	insertSamples(s, []Sample{0, 1, 10, 25, 100})
+	chkPct(t, s, 0, 0)
+	chkPct(t, s, .25, 1)
+	chkPct(t, s, .5, 10)
+	chkPct(t, s, .75, 25)
+	chkPct(t, s, 1, 100)
+
+	s = NewStats()
+	insertSamples(s, []Sample{25})
+	chkPct(t, s, 0, 25)
+	chkPct(t, s, .25, 25)
+	chkPct(t, s, .5, 25)
+	chkPct(t, s, .75, 25)
+	chkPct(t, s, 1, 25)
+
+	s = NewStats()
+	insertSamples(s, []Sample{1, 2})
+	chkPct(t, s, 0, 1)
+	chkPct(t, s, .25, 1)
+	chkPct(t, s, .5, 2)
+	chkPct(t, s, .75, 2)
+	chkPct(t, s, 1, 2)
+}
+
+type binTest struct {
+	samples  []Sample
+	binCount int
+	low      Sample
+	high     Sample
+	bins     []Sample
+}
+
+var binTests = []binTest{
+	{
+		samples:  []Sample{1, 2, 3},
+		binCount: 3,
+		low:      1,
+		high:     3,
+		bins:     []Sample{1, 3, math.MaxFloat64},
+	},
+	{
+		samples:  []Sample{1, 2, 3, 4},
+		binCount: 4,
+		low:      1,
+		high:     4,
+		bins:     []Sample{1, 2.5, 4, math.MaxFloat64},
+	},
+	{
+		samples:  []Sample{1, 2, 3, 4},
+		binCount: 5,
+		low:      1,
+		high:     4,
+		bins:     []Sample{1, 2, 3, 4, math.MaxFloat64},
+	},
+	{
+		samples:  []Sample{-100, -75, -50},
+		binCount: 4,
+		low:      -100,
+		high:     -50,
+		bins:     []Sample{-100, -75, -50, math.MaxFloat64},
+	},
+}
+
+type discardBinTest struct {
+	samples  []Sample
+	binCount int
+	discard  float64
+	bins     []Sample
+}
+
+var discardBinTests = []discardBinTest{
+	{
+		samples: []Sample{
+			-1000000,
+			1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+			1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+			1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+			1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+			1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+			1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+			1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+			1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+			1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+			1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+			1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+			1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+			1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+			1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+			1000000,
+		},
+		binCount: 11,
+		discard:  0.01,
+		bins:     []Sample{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, math.MaxFloat64},
+	},
+	{
+		samples: []Sample{
+			-1000000,
+			10, 20, 30, 40, 50, 60, 70, 80, 90, 100,
+			10, 20, 30, 40, 50, 60, 70, 80, 90, 100,
+			10, 20, 30, 40, 50, 60, 70, 80, 90, 100,
+			10, 20, 30, 40, 50, 60, 70, 80, 90, 100,
+			10, 20, 30, 40, 50, 60, 70, 80, 90, 100,
+			10, 20, 30, 40, 50, 60, 70, 80, 90, 100,
+			10, 20, 30, 40, 50, 60, 70, 80, 90, 100,
+			10, 20, 30, 40, 50, 60, 70, 80, 90, 100,
+			10, 20, 30, 40, 50, 60, 70, 80, 90, 100,
+			10, 20, 30, 40, 50, 60, 70, 80, 90, 100,
+			10, 20, 30, 40, 50, 60, 70, 80, 90, 100,
+			10, 20, 30, 40, 50, 60, 70, 80, 90, 100,
+			10, 20, 30, 40, 50, 60, 70, 80, 90, 100,
+			10, 20, 30, 40, 50, 60, 70, 80, 90, 100,
+			1000000,
+		},
+		binCount: 11,
+		discard:  0.01,
+		bins:     []Sample{10, 20, 30, 40, 50, 60, 70, 80, 90, 100, math.MaxFloat64},
+	},
+}
+
+func TestDiscardBins(t *testing.T) {
+	for _, test := range discardBinTests {
+		s := NewStats()
+		insertSamples(s, test.samples)
+		s.CreateBinsDiscard(test.binCount, test.discard)
+		if len(s.bins) != len(test.bins) {
+			t.Errorf("not enough bins: %d, expected %d", len(s.bins), len(test.bins))
+		}
+		for i, bin := range s.bins {
+			if bin != test.bins[i] {
+				t.Errorf("s.bins[%d] != %v, expected %v", i, bin, test.bins[i])
+			}
+		}
+	}
+}
